0c091d5f2bbba70e610a11931bb6bfd0d73bcb5f
Murtaza Badam <murtazabadam@gmail.com>
2026-09-15 16:42:05 +0530

    Add user-check helper script

---
+++ prisma/checkUser.js
@@ -0,0 +1,17 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+  const email = 'junaidnazeer88@gmail.com';
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    console.log(`No user found with email: ${email}`);
+    console.log('He needs to sign up first before he can log in.');
+  } else {
+    console.log('User found:');
+    console.log({ id: user.id, email: user.email, name: user.name, role: user.role });
+  }
+}
+
+main().catch(console.error).finally(() => prisma.$disconnect());
